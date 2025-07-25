be2ba4e21989af40451d7de62daccf4a44eaa5c3
rafaelSwi <rafanswi@protonmail.com>
2025-07-25 13:42:04 -0300

    Initial Commit

---
+++ ActivateMacOverlay/ActivateMacOverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivateMacOverlayApp.swift
+//  ActivateMacOverlay
+//
+//  Created by Rafael Neuwirth Swierczynski on 25/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ActivateMacOverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
